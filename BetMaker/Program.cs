b7a75f814daa22678c444d22e9c4c0d94ba85db4
Даниил <danilka.volkov.02@mail.ru>
2023-04-06 17:54:25 +0300

    Добавил бэкап дб

--- BetMaker/Program.cs
+++ BetMaker/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,20 @@ namespace BetMaker
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            string fileBackup = string.Concat(DateTime.Now.ToString("dd.MM.yyyy"), ".db"),
+                dirBackup = "Backup",
+                pathBackup = Path.Combine(dirBackup, fileBackup);
+
+            if (Directory.Exists(dirBackup) == false)
+            {
+                Directory.CreateDirectory(dirBackup);
+            }
+
+            if (File.Exists(pathBackup) == false)
+            {
+                File.Copy(Settings.PathDatabase, pathBackup);
+            }
 
             Application.Run(new MainForm());
         }
